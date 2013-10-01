c80f5c215ef7a90266f10acf42f3ff86b3420a8b
winesmoker <winesmoker@mail.ru>
2013-10-01 05:20:21 +0400

    sortirovka

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+         static void Main(string[]args)
+        {
+            int[]a={5,6,3,5,7};
+            int n=5;
+            int k;
+            int i=0;
+            for(i=0;i<n;i++)
+                for(int j=i+1;j<n;j++)
+                    if(a[i]>a[j])
+                    {
+                        k=a[j];
+                        a[j]=a[i];
+                        a[i]=k;
+                    }
+
+            for(i=0;i<n;i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+
+        }
+    }
+}
+
